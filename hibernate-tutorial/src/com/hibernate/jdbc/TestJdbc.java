55733a0e1d58b84e92058112cbce7240a65a645f
pramodaya <pramodayajayalath@gmail.com>
2019-02-19 23:42:36 +0530

    Hibernate Environment set up
    1.connected to db
    2.use annotation to map class entity to existing database.

---
+++ hibernate-tutorial/src/com/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String usr = "hbstudent";
+		String pass = "hbstudent";		
+
+		try {
+			
+			System.out.println("coneection to db: "+jdbcUrl);
+			Connection myconn = DriverManager.getConnection(jdbcUrl,usr,pass);
+			System.out.println("connection success");
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
